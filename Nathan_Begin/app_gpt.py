b62728fb66367cb56dd680325abfc97b032db029
davidwood3 <davidwood3@gmail.com>
2024-09-17 20:33:02 -0700

    First Commit

---
+++ Nathan_Begin/app_gpt.py
@@ -0,0 +1,15 @@
+from decouple import config
+from langchain_openai import ChatOpenAI
+
+OPENAI_API_KEY = config("OPENAI_API_KEY")
+
+llm = ChatOpenAI(temperature=0, model_name="gpt-4o", openai_api_key=OPENAI_API_KEY)
+
+print("Q & A with AI")
+print("=============")
+
+question ="What is the currency of Thailand?"
+print("Question: " + question)
+
+response = llm.invoke(question)
+print("Answer: " + response.content)
